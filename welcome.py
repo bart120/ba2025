57f394c4debf0761337b3436c778dbd1a4be4012
Vincent Leclerc <vleclerc@inow.fr>
2025-06-02 11:55:24 +0200

    init

---
+++ welcome.py
@@ -0,0 +1,13 @@
+# Déclaration variables
+
+prenom = "Alice"
+age = 30
+salaire = 2500.75
+actif = True
+# Affichage des variables
+print(prenom, age)
+
+# opérations simples
+heure_par_semaine = 35
+taux_horaire = salaire / heure_par_semaine
+print("Taux horaire : ", round(taux_horaire, 2))

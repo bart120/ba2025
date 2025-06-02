87791b8e5a96da74ac645cf24edddae48d9871df
Vincent Leclerc <vleclerc@inow.fr>
2025-06-02 12:36:33 +0200

    tableau & matrice

--- welcome.py
+++ welcome.py
@@ -11,3 +11,28 @@ print(prenom, age)
 heure_par_semaine = 35
 taux_horaire = salaire / heure_par_semaine
 print("Taux horaire : ", round(taux_horaire, 2))
+
+# conditions
+if age > 45:
+    print("Employé senior")
+else:
+    print("Employé junior")
+
+# itérations
+services = ["Développement", "Marketing", "Ventes"]
+print(services)
+for service in services:
+    print("Service :", service)
+
+index = 0
+while index < len(services):
+    print("Service :", services[index])
+    index += 1 # index = index + 1
+
+# matrices
+matrice = [
+    [5, 10, 15],
+    [8, 18, 2],
+    [7, 8, 9]
+]
+print(matrice[1][1])
